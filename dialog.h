1aed67a3b4bdfcf5dc0d768b3fbd82b41b626dcd
YoullloveGitHubUnlimitedCollaborators <alexander.zuz650501@gmail.com>
2018-06-06 22:36:20 +0300

    Second commit

--- dialog.h
+++ dialog.h
@@ -1,10 +1,16 @@
 #ifndef DIALOG_H
 #define DIALOG_H
 
+
 #include <QDialog>
+#include <QTcpSocket>
+#include <QHostAddress>
+#include "D:\Client-Server\SErver\myclient.h"
+#include <QMessageBox>
+class MyClient;
 
 namespace Ui {
-class Dialog;
+    class Dialog;
 }
 
 class Dialog : public QDialog
@@ -15,8 +21,24 @@ public:
     explicit Dialog(QWidget *parent = 0);
     ~Dialog();
 
+private slots:
+    void onSokConnected();
+    void onSokDisconnected();
+    void onSokReadyRead();
+    void onSokDisplayError(QAbstractSocket::SocketError socketError);
+
+    void on_pbConnect_clicked();
+    void on_pbDisconnect_clicked();
+    void on_cbToAll_clicked();
+    void on_pbSend_clicked();
+
 private:
     Ui::Dialog *ui;
+    QTcpSocket *_sok;
+    quint16 _blockSize;
+    QString _name;
+    void AddToLog(QString text, QColor color = Qt::black);
+
 };
 
-#endif // DIALOG_H fefefefef efef
+#endif // DIALOG_H
